c2f6f4e4612740feacd93c84958245e7ce6113f3
William Soliman <williamsoliman@williams-macbook-pro.local>
2018-07-03 10:17:54 +0800

    Todo list complete with datasource and delegate methods.

---
+++ Todoey/TodoListViewController.swift
@@ -0,0 +1,61 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by William Soliman on 2/7/18.
+//  Copyright © 2018 William Soliman. All rights reserved.
+//
+
+import UIKit
+
+// Replace the conventional UIViewController with a UITableViewController
+// Delegate and datasource declarations for the table view may be omitted
+class TodoListViewController: UITableViewController {
+
+    let itemArray = ["Find Mike", "Buy Eggos", "Destroy Demogorgon"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    //MARK - TableView DataSource Methods
+    
+    // Set number of rows equal to number of items in the array
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return itemArray.count
+        
+    }
+    
+    // Specify the cell for each row in the table view
+    // The identifier of the cell is specified in the Attributes inspector (after clicking on "Prototype Cells" in the storyboard)
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        
+        cell.textLabel?.text = itemArray[indexPath.row]
+        
+        return cell
+        
+    }
+    
+    //MARK - TableView Delegate Methods
+    
+    // When a given row is selected, print its contents
+    // Avoid the selected row from remaining highlighted (by deselecting it)
+    // Toggle a checkmark next to the selected cell (note that the checkmark accessory may be added to the ToDoItemCell under the Attributes inspector)
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+//        print(itemArray[indexPath.row])
+        
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        } else {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+
+}
+
